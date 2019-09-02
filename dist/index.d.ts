b420bba155dcc912e5b94fa40e49c7e7d77c15ae
arturwojnar-cyberus <54580606+arturwojnar-cyberus@users.noreply.github.com>
2019-09-02 16:41:29 +0200

    CYB-425 Themes (#1)

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,29 @@
-import './styles/widget.scss';
+import { Cash } from 'cash-dom/dist/cash.d';
 import { GeoProvider } from "cyberuskey-sdk";
+import './styles/widget.scss';
 export * from "cyberuskey-sdk";
+/**
+ * Defines the widget animation.
+ *
+ * @export
+ * @enum {number}
+ */
+export declare enum WidgetAnimation {
+    None = 0,
+    Blinking = 1,
+    Waves = 2
+}
+/**
+ * Widget options passed to the its constructor.
+ *
+ * @export
+ * @interface WidgetOptions
+ */
+export interface WidgetOptions {
+    theme?: string;
+    serverUrl?: string;
+    animation?: WidgetAnimation;
+}
 /**
  * Class represents a UI button that uses `cyberuskey-sdk` and allows to make a login with Cyberus Key Authentication Server.
  *
@@ -11,8 +34,12 @@ export * from "cyberuskey-sdk";
  * import { CyberusKeyWidget, HTML5GeoProvider } from "cyberuskey-widget";
  *
  * $(document).ready(() => {
- *  const cyberusKeyButton = new CyberusKeyWidget('default', API_URL);
- *  cyberusKeyButton.create('.cyberus-key-widget-container', CLIENT_ID, REDIRECT_URI, new HTML5GeoProvider());
+ *   const cyberusKeyButton = new CyberusKeyWidget({
+ *     theme: 'default',
+ *     serverUrl: API_URL
+ *   };
+ *
+ *   cyberusKeyButton.create('.cyberus-key-widget-container', CLIENT_ID, REDIRECT_URI, new HTML5GeoProvider());
  * });
  * ```
  *
@@ -29,21 +56,23 @@ export declare class CyberusKeyWidget {
     private _nonce;
     private _initialized;
     private _inProgress;
+    private _containingElementSelector;
+    private _animation;
     /**
      * Creates an instance of CyberusKeyWidget.
      *
-     * @param {string} [theme='default'] A theme of the button.
-     * @param {string} [serverUrl='https://auth-server-demo.cyberuslabs.net'] Cyberus Key Authentication server URL.
+     * @param {WidgetOptions} [options={}] Widget options.
      * @memberof CyberusKeyWidget
      */
-    constructor(theme?: string, serverUrl?: string);
+    constructor(options?: WidgetOptions);
     /**
      * Creates a Cyberus Key button element in the DOM tree.
      *
      * @param {string} containingElementSelector Selector of a containing DOM element for the button.
      * @param {string} clientId Public client ID generated during creating the account.
      * @param {string} redirectUri Redirect URI to which the response will be sent. If the value is not whitelisted then the request will fail.
-     * @param {GeoProvider} [geoProvider] Provider of a geolocalization. For a web browser use HTML5GeoProvider.
+     * @param {GeoProvider} [geoProvider] Provider of a geolocalization. `If passed, then geolocalization measurement will be taken`.
+     *    For a web browser use HTML5GeoProvider.
      *    Geolocalization measurement can be later use to compare it against the mobile's measurement (if you have set `fail_on_geo_mismatch`).
      *    Those measurements can be used also to general improvement of the security.
      * @param {string} [state]
@@ -60,4 +89,15 @@ export declare class CyberusKeyWidget {
     create(containingElementSelector: string, clientId: string, redirectUri: string, geoProvider?: GeoProvider, state?: string, nonce?: string): void;
     _loginButtonClick(): Promise<void>;
     _getUrl(path: string): string;
+    _getElement(selector: string): Cash;
+    _loading(): void;
+    _noLoading(): void;
+    _disable(): void;
+    _enable(): void;
+    _blink(): void;
+    _stopBlinking(): void;
+    _waves(): void;
+    _stopWaves(): void;
+    _animate(): void;
+    _stopAnimation(): void;
 }
